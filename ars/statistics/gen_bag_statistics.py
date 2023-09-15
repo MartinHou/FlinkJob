c14dd287e67a07f6c36f743a98ef15e81a27cf55
martin.hou <martin.hou@momenta.ai>
2023-09-15 18:27:51 +0800

    [fmt] fmt: reformat and rearrange

--- ars/statistics/gen_bag_statistics.py
+++ ars/statistics/gen_bag_statistics.py
@@ -45,30 +45,30 @@ class AddTimeProcess(ProcessFunction):
 class HandleDurationFlatMap(FlatMapFunction):
     def __init__(self, tag: str = 'noraml') -> None:
         self.tag = tag
-        self.count_timer = None  # 分钟级:return_dict
-        self.day_count_timer = None,  # day级: return_dict-minutes级时间戳
-        self.day_temp_timer = None,  # day级:占位的1?
+        self.minute_data = None  # 分钟0秒stamp: return_dict
+        self.day_data = None,  # 天0点stamp: return_dict
+        self.changed_days = None,  # 修改但未持久化的天stamp作为key，当hashset用
         self.delay_time = 86400
 
     def open(self, context: RuntimeContext) -> None:
         descriptor = MapStateDescriptor(
-            name='count_timer',
+            name='minute_data',
             key_type_info=Types.INT(),
             value_type_info=Types.STRING(),
         )
-        self.count_timer = context.get_map_state(descriptor)
+        self.minute_data = context.get_map_state(descriptor)
         descriptor1 = MapStateDescriptor(
-            name='day_count_timer',
+            name='day_data',
             key_type_info=Types.INT(),
             value_type_info=Types.STRING(),
         )
-        self.day_count_timer = context.get_map_state(descriptor1)
+        self.day_data = context.get_map_state(descriptor1)
         descriptor2 = MapStateDescriptor(
-            name='day_temp_timer_name',
+            name='changed_days_name',
             key_type_info=Types.INT(),
             value_type_info=Types.INT(),
         )
-        self.day_temp_timer = context.get_map_state(descriptor2)
+        self.changed_days = context.get_map_state(descriptor2)
 
     def flat_map(self, value):
         return_dict = {
@@ -79,90 +79,38 @@ class HandleDurationFlatMap(FlatMapFunction):
             'minutetime': value['minutetime'],
             'minutetime_int': value['minutetime_int'],
         }
-        count_json = self.count_timer.get(value['minutetime_int'])
-        if self.count_timer.is_empty():
-            max_time_int = 0
-        else:
-            max_time_int = max(list(self.count_timer.keys()))  # 目前为止最大的分钟时间戳
+        count_json = self.minute_data.get(value['minutetime_int'])
+        max_minute_int = max(self.minute_data.keys(
+        )) if not self.minute_data.is_empty() else 0  # 目前为止最大的分钟时间戳
+        
+        if max_minute_int - value[
+                'minutetime_int'] > self.delay_time:  # 过期数据，丢弃
+            return iter([])
 
         if count_json is None:  # 首次进入这个60秒窗口
+            self.minute_data.put(value['minutetime_int'],
+                                 json.dumps(return_dict))
+            if max_minute_int == 0:  # kafka来的第一条数据
+                return iter([])
+            if value[
+                    'minutetime_int'] > max_minute_int and max_minute_int != 0:  # 这是更大的分钟，把前一分钟数据持久化
+                self.record_minute(self.minute_data.get(max_minute_int))
+            else:  # 不是最新的分钟
+                self.update_day_state(value)
 
-            if value['minutetime_int'] > max_time_int and max_time_int != 0:  # 这是更大的分钟
-                self.count_timer.put(value['minutetime_int'],
-                                     json.dumps(return_dict))
-                self.minute_to_day(self.count_timer.get(max_time_int))
-                self.day_temp_timer.clear()
-
-            elif max_time_int == 0:  # 没有旧数据
-                self.count_timer.put(value['minutetime_int'],
-                                     json.dumps(return_dict))
-            elif max_time_int - value[
-                    'minutetime_int'] <= self.delay_time:  # 在以前的时间，且不超1天前
-                self.count_timer.put(value['minutetime_int'],
-                                     json.dumps(return_dict))
-                self.value_to_day(value)
-
-        else:  # 该分钟有过数据了
+        else:  # 该分钟有过数据了，count_json先更新为加上这次数据的这分钟数据
             count_json = json.loads(count_json)
             count_json['duration'] += value['duration']
+            self.minute_data.put(value['minutetime_int'],
+                                 json.dumps(count_json))
+            if value['minutetime_int'] != max_minute_int:  # 不是的最新的分钟
+                self.update_day_state(value)
+        return iter([])
 
-            if value['minutetime_int'] == max_time_int:  # 当前分钟时最新的分钟
-                self.count_timer.put(value['minutetime_int'],
-                                     json.dumps(count_json))
-            elif max_time_int - value[
-                    'minutetime_int'] <= self.delay_time:  # 当前分钟不是最新的分钟，且不超1天前
-                self.count_timer.put(value['minutetime_int'],
-                                     json.dumps(count_json))
-                self.value_to_day(value)
-        yield value
-
-    def minute_to_day(self, minute_json: str):
-        minute_json = json.loads(minute_json)
-        daytime_int = minute_json['daytime_int']
-        day_json = self.day_count_timer.get(daytime_int)
-        self.day_temp_timer.put(daytime_int, 1)
-        if day_json == None:
-            self.day_count_timer.put(
-                daytime_int,
-                json.dumps({
-                    'daytime_int': daytime_int,
-                    'daytime': minute_json['daytime'],
-                    'label': minute_json['label'],
-                    'duration': minute_json['duration'],
-                }))
-        else:
-            day_json = json.loads(day_json)
-            day_json['duration'] += minute_json['duration']
-            self.day_count_timer.put(daytime_int, json.dumps(day_json))
-        for daytime_int_one in self.day_temp_timer.keys():
-
-            self.stat_duration(
-                json_str=self.day_count_timer.get(daytime_int_one),
-                tag=self.tag)
-
-    def value_to_day(self, value: dict):
-        daytime_int = value['daytime_int']
-        day_json = self.day_count_timer.get(daytime_int)
-        self.day_temp_timer.put(daytime_int, 1)
-        if day_json == None:
-            self.day_count_timer.put(
-                daytime_int,
-                json.dumps({
-                    'daytime_int': daytime_int,
-                    'daytime': value['daytime'],
-                    'label': value['label'],
-                    'duration': value['duration'],
-                }))
-        else:
-            day_json = json.loads(day_json)
-            day_json['duration'] += value['duration']
-            self.day_count_timer.put(daytime_int, json.dumps(day_json))
-
-    def stat_duration(self, json_str, tag: str):
+    def stat_duration(self, json_str):
         json_mysql = json.loads(json_str)
         stat_date = json_mysql['daytime'] + ' 00:00:00'
-        # name=tag.replace('status','success')
-        name = tag
+        name = self.tag
         period = 'daily'
         statis_one = StatisticsActions()
         list_get = list(
@@ -178,6 +126,33 @@ class HandleDurationFlatMap(FlatMapFunction):
             info[json_mysql['label']] = json_mysql['duration']
             statis_one.update_statistics(
                 name=name, period=period, stat_date=stat_date, info=info)
+            
+    def record_minute(self, minute_json: str):
+        minute_json = json.loads(minute_json)
+        self.update_day_state(minute_json)
+        for daytime_int_one in self.changed_days.keys():
+            self.stat_duration(self.day_data.get(daytime_int_one))
+        self.changed_days.clear()
+
+    def update_day_state(self, new_dict: dict):
+        daytime_int = new_dict['daytime_int']
+        day_json = self.day_data.get(daytime_int)
+        self.changed_days.put(daytime_int, 1)
+        if day_json == None:
+            self.day_data.put(
+                daytime_int,
+                json.dumps({
+                    'daytime_int': daytime_int,
+                    'daytime': new_dict['daytime'],
+                    'label': new_dict['label'],
+                    'duration': new_dict['duration'],
+                }))
+        else:
+            day_json = json.loads(day_json)
+            day_json['duration'] += new_dict['duration']
+            self.day_data.put(daytime_int, json.dumps(day_json))
+
+    
 
 
 def analyse(env):
