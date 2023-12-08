ad466418ea17bb1c058cfc5655bec60042a55350
martin.hou <martin.hou@momenta.ai>
2023-12-08 11:29:20 +0800

    [cdc] feat: Add Flink CDC

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -5,3 +5,5 @@ flink run -d -py ars/mxf/filter_bag_level_crash.py
 flink run -d -py ars/statistics/pod_stat.py -pyfs /mnt/data/userdata/martin.hou/flink-jobs
 flink run -d -py ars/statistics/bag_stat.py -pyfs /mnt/data/userdata/martin.hou/flink-jobs
 
+flink run -d -py ars/data_migration/migrate_workflow.py
+flink run -d -py ars/data_migration/migrate_result.py
