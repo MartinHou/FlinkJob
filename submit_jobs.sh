93a7b99f88be94c8a48c3a8190455e1bc04e23c6
martin.hou <martin.hou@momenta.ai>
2023-12-08 14:14:35 +0800

    [sh] fix: Modify pyfs path

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,8 +2,8 @@ flink run -d -py ars/warning/pod_err_monitor.py
 flink run -d -py ars/warning/node_monitor.py
 flink run -d -py ars/mxf/filter_bag_level_crash.py
 
-flink run -d -py ars/statistics/pod_stat.py -pyfs /mnt/data/userdata/martin.hou/flink-jobs
-flink run -d -py ars/statistics/bag_stat.py -pyfs /mnt/data/userdata/martin.hou/flink-jobs
+flink run -d -py ars/statistics/pod_stat.py -pyfs /mnt/data/martin.hou/flink-jobs
+flink run -d -py ars/statistics/bag_stat.py -pyfs /mnt/data/martin.hou/flink-jobs
 
 flink run -d -py ars/data_migration/migrate_workflow.py
 flink run -d -py ars/data_migration/migrate_result.py
